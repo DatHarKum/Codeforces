ef51d7e1c58cacb48e27e88f1d3c65e86e1e00a4
DatHarKum <80124861+DatHarKum@users.noreply.github.com>
2021-04-13 14:57:15 +0530

    Create 999_A_mishka_and_contest.cpp

---
+++ 999_A_mishka_and_contest.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    int a[n+1];
+    for(int i = 0 ; i < n ; i++)
+    {
+        int x;
+        cin >> x;
+        a[i] = x;
+    }
+    int p = 0;
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(a[i] > k)break;
+ 
+        p++;
+    }
+    int q = 0;
+    for(int i = n-1 ; i >= 0 ; i--)
+    {
+        if(a[i] > k)break;
+ 
+        q++;
+    }
+    if(n-q<p){cout <<  n;return 0;}
+ 
+    cout << p+q;
+ 
+    return 0;
+ 
